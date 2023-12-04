71c5029bb841c57bbe33f2cada8d0aa3c5f40192
steven-jacovitch <55928898+steven-jacovitch@users.noreply.github.com>
2023-12-04 00:04:42 -0500

    Add files via upload

---
+++ js/final.js
@@ -0,0 +1,21 @@
+document.querySelector("#classBttn").addEventListener("click", function() {
+    var x = document.getElementById("charStats");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+        document.getElementById("classBttn").innerHTML = "Hide Base Stats";
+    } else {
+        x.style.display = "none";
+        document.getElementById("classBttn").innerHTML = "Show Base Stats";
+    }
+});
+
+document.querySelector("#classProfs").addEventListener("click", function() {
+    var x = document.getElementById("charProfs");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+        document.getElementById("classProfs").innerHTML = "Hide Base Proficiencies";
+    } else {
+        x.style.display = "none";
+        document.getElementById("classProfs").innerHTML = "Show Base Proficiencies";
+    }
+});
